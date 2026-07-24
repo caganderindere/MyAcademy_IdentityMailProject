8d12eed001fbe019d5ca56d63ea2fcc91751c007
Çağan Derindere <cederindere@hotmail.com>
2026-07-24 14:23:51 +0300

    Proje dosyası ekle.

---
+++ MyAcademy_IdentityMailProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAcademy_IdentityMailProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
